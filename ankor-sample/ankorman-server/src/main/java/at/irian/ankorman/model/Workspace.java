fb4291f5c20f45604a000a5eccf5e020d91f95fe
Manfred Geiler <manfred.geiler@irian.at>
2013-06-20 14:45:45 +0200

    refactoring (cleanup)

--- ankor-sample/ankorman-server/src/main/java/at/irian/ankorman/model/Workspace.java
+++
@@ -1,8 +0,0 @@
-package at.irian.ankorman.model;
-
-/**
- * @author MGeiler (Manfred Geiler)
- */
-public class Workspace {
-    //private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(Workspace.class);
-}
