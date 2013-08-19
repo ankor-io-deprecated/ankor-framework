43ab5442757f684e6285be3049f4053fcf03e4a0
Florian Klampfer <florian.klampfer@irian.at>
2013-08-19 11:43:27 +0200

    replace listview with custom solution

--- ankor-sample-todo/ankor-sample-todo-javafx-client/src/main/java/at/irian/ankor/todosample/fxclient/task/TaskListController.java
+++ ankor-sample-todo/ankor-sample-todo-javafx-client/src/main/java/at/irian/ankor/todosample/fxclient/task/TaskListController.java
@@ -5,19 +5,19 @@ import at.irian.ankor.fx.binding.BindingContext;
 import at.irian.ankor.ref.Ref;
 import at.irian.ankor.ref.listener.RefChangeListener;
 import at.irian.ankor.ref.listener.RefListeners;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.util.Callback;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.ResourceBundle;
 
-import static at.irian.ankor.fx.binding.ValueBindingsBuilder.bindSubValues;
 import static at.irian.ankor.fx.binding.ValueBindingsBuilder.bindValue;
 import static at.irian.ankor.todosample.fxclient.App.refFactory;
 
@@ -27,7 +27,7 @@ public class TaskListController implements Initializable {
     private BindingContext bindingContext = new BindingContext();
     private Ref modelRef;
 
-    @FXML public ListView tasksList;
+    @FXML public VBox tasksList;
     @FXML public ToggleButton toggleAll;
     @FXML public TextField newTodo;
     @FXML public Label todoCountNum;
@@ -54,14 +54,16 @@ public class TaskListController implements Initializable {
         });
 
         refFactory().rootRef().fire(new Action("init"));
-
     }
 
     public void initialize() {
-        // XXX: tree change listener is needed
-        bindSubValues(modelRef.append("tasks"))
-                .toProperty(tasksList.itemsProperty())
-                .createWithin(bindingContext);
+        RefListeners.addPropChangeListener(modelRef.append("tasks"), new RefChangeListener() {
+            @Override
+            public void processChange(Ref changedProperty) {
+                List<LinkedHashMap<String, Object>> tasks = changedProperty.getValue();
+                Platform.runLater(new TaskLoader(tasks));
+            }
+        });
 
         bindValue(modelRef.append("toggleAll"))
                 .toProperty(toggleAll.selectedProperty())
@@ -103,13 +105,6 @@ public class TaskListController implements Initializable {
         bindValue(modelRef.append("filterCompletedSelected"))
                 .toProperty(filterCompleted.selectedProperty())
                 .createWithin(bindingContext);
-
-        tasksList.setCellFactory(new Callback<ListView, ListCell>() {
-            @Override
-            public ListCell call(ListView listView) {
-                return new TaskComponentListCell();
-            }
-        });
     }
 
     @FXML
@@ -134,32 +129,22 @@ public class TaskListController implements Initializable {
         modelRef.fire(new Action("toggleAll"));
     }
 
-    HashMap<Object, TaskPane> cache = new HashMap<Object, TaskPane>();
-    private class TaskComponentListCell extends ListCell<LinkedHashMap<String, Object>> {
+    private class TaskLoader implements Runnable {
+
+        List<LinkedHashMap<String, Object>> tasks;
+        public TaskLoader(List<LinkedHashMap<String, Object>> tasks) { this.tasks = tasks; }
 
         @Override
-        public void updateItem(LinkedHashMap<String, Object> item, boolean empty) {
-            super.updateItem(item, item == null);
-            if (item != null) {
-                //Object id = item.get("id");
-                int index = getIndex();
-                String title = (String)item.get("title");
-                boolean completed = (boolean)item.get("completed");
+        public void run() {
+            tasksList.getChildren().clear();
 
-                // XXX: Memory Leak
-                if (cache.get(index) == null) {
-                    cache.put(index, new TaskPane(modelRef));
-                }
-                TaskPane node = cache.get(index);
-
-                node.setText(title);
-                node.setSelected(completed);
-                node.setIndex(index);
-
-                setGraphic(node);
-            } else {
-                // XXX: Client must know how to handle null values
-                setGraphic(null);
+            int i = 0;
+            for (LinkedHashMap<String, Object> task : tasks) {
+                TaskPane node = new TaskPane(modelRef);
+                node.setIndex(i++);
+                node.setText((String)task.get("title"));
+                node.setSelected((boolean)task.get("completed"));
+                tasksList.getChildren().add(node);
             }
         }
     }
