1a535535ebe73e505901b92c80087d4cf8c75393
Florian Klampfer <florian.klampfer@irian.at>
2013-08-20 15:58:00 +0200

    js todo sample app bindings

--- ankor-sample-todo/ankor-sample-todo-js-client/src/main/webapp/js/sample-todo/TaskList.js
+++ ankor-sample-todo/ankor-sample-todo-js-client/src/main/webapp/js/sample-todo/TaskList.js
@@ -1,104 +1,97 @@
 define([
     "jquery",
-    "text!./templates/TaskList.html"
-], function($, template) {
+    "text!./templates/TaskList.html",
+    "text!./templates/Task.html"
+], function($, template, taskTemplate) {
     template = new EJS({
         text: template
     });
 
+    taskTemplate = new EJS({
+        text: taskTemplate
+    });
+
     var TaskList = function(modelRef) {
 
-        var modelVal = modelRef.getValue();
+        // Hacky little helpers
+        $.fn.addOrRemoveClass = function(clazz, isSelected) {
+            var fun = isSelected ? $.fn.addClass : $.fn.removeClass;
+            fun.call(this, clazz);
+        };
 
-        var $app = $("#app");
-        $("body").html(template.render({}));
+        $.fn.addOrRemoveSelectedClass = function(isSelected) {
+            $.fn.addOrRemoveClass.call(this, "selected", isSelected);
+        };
 
-        $('#todo-count > strong').ankorBindInnerHTML(modelRef.append("itemsLeft"));
-        $('#todo-count > span').ankorBindInnerHTML(modelRef.append("itemsLeftText"));
-        $('#clear-completed').ankorBindInnerHTML(modelRef.append("itemsCompleteText"));
+        $.fn.ankorBind = function(fun, ref) {
+            var element = this.first();
+            return ref.addPropChangeListener(function() {
+                if ($.fn[fun] == null) {
+                    console.log("Error: $.fn has no function " + fun);
+                } else {
+                    $.fn[fun].call(element, ref.getValue());
+                }
+            });
+        }
 
-        // //Table rendering
-        // var listeners = [];
-        // modelRef.append("tasks").addPropChangeListener(function(ref) {
-        //     var rows = ref.getValue();
-        //     var tbody = body.find("tbody");
+        // var modelVal = modelRef.getValue(); // TODO: is this necessary?
 
-        //     //Cleanup
-        //     tbody.html("");
-        //     for (var i = 0, listener; (listener = listeners[i]); i++) {
-        //         listener.remove();
-        //     }
-        //     listeners = [];
+        $("#app").html(template.render({}));
 
-        //     //Render new table
-        //     for (var i = 0, row; (row = rows[i]); i++) {
-        //         tbody.append("<tr><td><input type='text' value='" + row.name + "'></td><td>" + row.type + "</td><td>" + row.family + "</td></tr>");
-        //         listeners.push(tbody.find("tr:last-child input").ankorBindInputValue(ref.appendIndex(i).append("name")));
-        //     }
-        // });
+        $('#todo-count > strong').ankorBind("html", modelRef.append("itemsLeft"));
+        $('#todo-count > span').ankorBind("html",  modelRef.append("itemsLeftText"));
 
-        var ENTER_KEY = 13;
-        var $newTask = $("#new-todo");
-        $newTask.keyup(function(e) {
-            if (e.keyCode === ENTER_KEY && $newTask.val() != "") {
-                modelRef.fire("newTask", { title: $newTask.val() });
+        $('#main').ankorBind("toggle", modelRef.append("footerVisibility"));
+        $('#footer').ankorBind("toggle", modelRef.append("footerVisibility"));
+
+        var $clearCompleted = $('#clear-completed');
+        $clearCompleted.ankorBind("html", modelRef.append("itemsCompleteText"));
+        $clearCompleted.ankorBind("toggle", modelRef.append("clearButtonVisibility"));
+
+        $('#filter-all').ankorBind("addOrRemoveSelectedClass", modelRef.append("filterAllSelected"));
+        $('#filter-active').ankorBind("addOrRemoveSelectedClass", modelRef.append("filterActiveSelected"));
+        $('#filter-completed').ankorBind("addOrRemoveSelectedClass", modelRef.append("filterCompletedSelected"));
+
+        modelRef.append("toggleAll").addPropChangeListener(function(ref) {
+            $("#toggle-all").prop("checked", ref.getValue());
+        });
+
+        var bindingContext = []
+        modelRef.append("tasks").addPropChangeListener(function(ref) {
+            //Cleanup
+            var $todoList = $('#todo-list').html('');
+            for (var i = 0, listener; (listener = bindingContext[i]); i++) {
+                listener.remove();
+            }
+            bindingContext = [];
+
+            var listRef = function(index) {
+                return ref.appendIndex(index);
+            }
+
+            var tasks = ref.getValue();
+            for (var i = 0, task; (task = tasks[i]); i++) {
+                // XXX: Task view ?
+                $todoList.append(taskTemplate.render(task));
+
+                bindingContext.push(
+                    $('#task-'+task.id+' label')
+                        .ankorBind("html", listRef(i).append("title")));
+
+                bindingContext.push(
+                    $('#task-'+task.id+' .edit')
+                        .ankorBind("html", listRef(i).append("title")));
             }
         });
 
-        //Create tab header
-        //   $("#tabs ul").append("<li class='" + tab.id + "'><a href='#tab-" + tab.id + "'><span></span></a><a href='#' class='close'>x</a></li>");
-        // $("#tabs ul li." + tab.id + " span").ankorBindInnerHTML(ref.append("name"));
-        // $("#tabs ul li." + tab.id + " a.close").click(function() {
-        //     ref.setValue(null);
-        // });
-
-        //Create tab body
-        // $("#tabs").append(template.render({
-        //     id: tab.id
-        // }));
-
-        // body.find("input.name").ankorBindInputValue(ref.append("model.filter.name"));
-        // body.find("select.type").ankorBindSelectItems(ref.append("model.selectItems.types"), {
-        //     emptyOption: true
-        // });
-        // body.find("select.type").ankorBindInputValue(ref.append("model.filter.type"));
-        // body.find("select.family").ankorBindSelectItems(ref.append("model.selectItems.families"), {
-        //     emptyOption: true
-        // });
-        // body.find("select.family").ankorBindInputValue(ref.append("model.filter.family"));
-        // body.find("button.previous").click(function() {
-        //     ref.append("model.animals.paginator").fire("previous");
-        // });
-        // body.find("button.next").click(function() {
-        //     ref.append("model.animals.paginator").fire("next");
-        // });
-        // body.find("button.save").click(function() {
-        //     ref.append("model").fire("save");
-        // });
-
-        //Table rendering
-        // var listeners = [];
-        // ref.append("model.animals.rows").addPropChangeListener(function(ref) {
-        //     var rows = ref.getValue();
-        //     var tbody = body.find("tbody");
-
-        //     //Cleanup
-        //     tbody.html("");
-        //     for (var i = 0, listener; (listener = listeners[i]); i++) {
-        //         listener.remove();
-        //     }
-        //     listeners = [];
-
-        //     //Render new table
-        //     for (var i = 0, row; (row = rows[i]); i++) {
-        //         tbody.append("<tr><td><input type='text' value='" + row.name + "'></td><td>" + row.type + "</td><td>" + row.family + "</td></tr>");
-        //         listeners.push(tbody.find("tr:last-child input").ankorBindInputValue(ref.appendIndex(i).append("name")));
+        // TODO: Params in Actions currently not supported
+        // var ENTER_KEY = 13;
+        // var $newTask = $("#new-todo");
+        // $newTask.keyup(function(e) {
+        //     if (e.keyCode === ENTER_KEY && $newTask.val() != "") {
+        //         modelRef.fire("newTask", { title: $newTask.val() });
         //     }
         // });
-
-        // //Refresh tabs and select newly created tab
-        // $("#tabs").tabs("refresh");
-        // $("#tabs").tabs("option", "active", $("#tabs > div").length - 1);
     };
 
     return TaskList;
