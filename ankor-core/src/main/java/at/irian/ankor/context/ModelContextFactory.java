5f136b64b29ec66dd840118029503d390297d105
Manfred Geiler <manfred.geiler@irian.at>
2014-02-11 15:52:23 +0100

    javadoc

--- ankor-core/src/main/java/at/irian/ankor/context/ModelContextFactory.java
+++ ankor-core/src/main/java/at/irian/ankor/context/ModelContextFactory.java
@@ -1,8 +1,15 @@
 package at.irian.ankor.context;
 
 /**
+ * Factory for ModelContext.
+ *
  * @author Manfred Geiler
  */
 public interface ModelContextFactory {
+    /**
+     * Create a new ModelContext with the given unique id.
+     * @param modelContextId  unique model id
+     * @return the newly created ModelContext
+     */
     ModelContext createModelContext(String modelContextId);
 }
--- ankor-core/src/main/java/at/irian/ankor/context/ModelContextManager.java
+++ ankor-core/src/main/java/at/irian/ankor/context/ModelContextManager.java
@@ -1,12 +1,27 @@
 package at.irian.ankor.context;
 
 /**
+ * The ModelContextManager handles all ModelContexts that are currently active.
+ * "Active" means that an application (i.e. user session) is currently connected to this model.
+ *
  * @author Manfred Geiler
  */
 public interface ModelContextManager {
 
+    /**
+     * Get the ModelContext with the given id. Create a new ModelContext (by means of a {@link ModelContextFactory})
+     * if there is none with this id yet.
+     * @param modelContextId  model id
+     * @return the existing (or a newly created) ModelContext with the given id
+     */
     ModelContext getOrCreate(String modelContextId);
 
+    /**
+     * Invalidate (i.e. close) the given ModelContext.
+     * ModelContextManager implementations must call the {@link at.irian.ankor.context.ModelContext#close()} method
+     * on the given instance and remove any reference to the ModelContext.
+     * @param modelContext  ModelContext to invalidate
+     */
     void invalidate(ModelContext modelContext);
 
 }
