d9fe8462c80ebe8c4880edc8efa645b3e89d830d
Manfred Geiler <manfred.geiler@irian.at>
2014-03-11 19:33:58 +0100

    CollaborationSingleRootApplication for Todo working again

--- samples/todo-sample/todo-application/src/main/java/at/irian/ankorsamples/todosample/server/TodoServerApplication.java
+++ samples/todo-sample/todo-application/src/main/java/at/irian/ankorsamples/todosample/server/TodoServerApplication.java
@@ -1,6 +1,6 @@
 package at.irian.ankorsamples.todosample.server;
 
-import at.irian.ankor.application.SimpleSingleRootApplication;
+import at.irian.ankor.application.CollaborationSingleRootApplication;
 import at.irian.ankor.ref.Ref;
 import at.irian.ankorsamples.todosample.domain.TaskRepository;
 import at.irian.ankorsamples.todosample.viewmodel.ModelRoot;
@@ -10,7 +10,7 @@ import java.util.Map;
 /**
  * @author Manfred Geiler
  */
-public class TodoServerApplication extends SimpleSingleRootApplication {
+public class TodoServerApplication extends CollaborationSingleRootApplication {
     //private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(AnimalsServerApplication.class);
 
     private static final String APPLICATION_NAME = "Todo Server";
@@ -21,9 +21,8 @@ public class TodoServerApplication extends SimpleSingleRootApplication {
     }
 
     @Override
-    public Object createModel(Ref rootRef, Map<String, Object> connectParameters) {
+    public Object doCreateModel(Ref rootRef, Map<String, Object> connectParameters) {
         TaskRepository animalRepository = new TaskRepository();
         return new ModelRoot(rootRef, animalRepository);
     }
-
 }
