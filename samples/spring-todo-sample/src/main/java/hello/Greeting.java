8e258eae8ab3ecf55301de2086fb5b9701dfe3bd
Florian Klampfer <florian.klampfer@irian.at>
2014-08-19 13:44:38 +0200

    spring websocket example

---
+++ samples/spring-todo-sample/src/main/java/hello/Greeting.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class Greeting {
+    
+    private String content;
+    
+    public Greeting(String content) {
+        this.content = content;
+    }
+    
+    public String getContent() {
+        return content;
+    }
+}
